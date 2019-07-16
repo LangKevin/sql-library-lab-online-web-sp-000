8dda9857f783c2efd02214e30ad8a1a33be271fc
kevin lang <langkevina@gmail.com>
2019-07-16 01:10:15 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "select species, count(*) as cnt from characters group by species order by cnt desc LIMIT 1 "
 end
 
 def select_name_and_series_subgenres_of_authors
